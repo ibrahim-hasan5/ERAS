646a6a3bd0ea7cd3bbbc3de678eba4edd3539c76
IsratMou <moutherobogirl@gmail.com>
2025-10-17 22:49:42 +0600

    1.blood donor Phone number view. 2) citizen pages +blood network page reponsive done

--- eras/accounts/views.py
+++ eras/accounts/views.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
