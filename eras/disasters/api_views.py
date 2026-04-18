e73f2a4d89924d92a3d8b53939d13ea6b5c585e7
ibrahim-hasan5 <ibrahimhasan0172@gmail.com>
2026-04-18 23:06:33 +0600

    update

--- eras/disasters/api_views.py
+++ eras/disasters/api_views.py
@@ -4,7 +4,10 @@ from rest_framework.response import Response
 from .models import Disaster, DisasterAlert, DisasterResponse, DisasterUpdate
 from .serializers import DisasterSerializer, DisasterAlertSerializer, DisasterResponseSerializer, DisasterUpdateSerializer
 
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+
 class DisasterViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
     queryset = Disaster.objects.filter(is_active=True).order_by('-created_at')
     serializer_class = DisasterSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -14,6 +17,7 @@ class DisasterViewSet(viewsets.ModelViewSet):
 
 class DisasterAlertViewSet(viewsets.ModelViewSet):
     serializer_class = DisasterAlertSerializer
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
